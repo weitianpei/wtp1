26e0824f93fafc704b755c5552f74bffae056241
weitianpei <342351837@qq.com>
2021-03-27 10:12:30 +0800

    add weitianpei

---
+++ src/main/java/com/niujiaozai/People.java
@@ -0,0 +1,14 @@
+package com.niujiaozai;
+
+public class People {
+
+    private double age;
+
+    public double getAge() {
+        return age;
+    }
+
+    public void setAge(double age) {
+        this.age = age;
+    }
+}
